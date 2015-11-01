7910a6438f1d9d6a33bdce3f57615aa1d770a0d1
196884 <regis.dupont+git@m4x.org>
2015-11-01 14:32:55 -0500

    Retrieving data via twisted.web.client

--- Coinbase/coinbase_web.py
+++ Coinbase/coinbase_web.py
@@ -1,72 +1,121 @@
-import ConfigParser
-import requests
-from twisted.internet import reactor
-from twisted.web.client import getPage
-from twisted.application.service import Application
-from twisted.python.log import ILogObserver, FileLogObserver
-from twisted.python import log, util
-from twisted.python.logfile import DailyLogFile
-from autobahn.twisted.websocket import WebSocketClientFactory, WebSocketClientProtocol, connectWS
 import sys
-import json
-from decimal import Decimal
-import decimal
+from twisted.internet import reactor
 
-decimal.getcontext().prec = 20
+def stop(msg = ""):
+    if len(msg) > 0:
+        suffix = " [{0}]".format(msg)
+    else:
+        suffix = ""
+    print "stop - stopping reactor{0}".format(suffix)
+    reactor.stop()
 
-def formatBookSideL3(bs):
-    result = []
-    quantity = 0
-    price = None
-    for [p, s, oid] in bs:
-        pd = Decimal(p)
-        sd = Decimal(s)
-        if price != pd and price is not None:
-            result.append((price, quantity))
-        price = pd
-        quantity = quantity + sd
-    result.append((price, quantity))
-    return result
+def started():
+    print "started - reactor is running"
+    stop("finished")
 
-def formatBookL3(book):
-    bids = formatBookSideL3(book["bids"])
-    asks = formatBookSideL3(book["asks"])
-    for (p, q) in reversed(bids):
-        print "{0}x{1}".format(p, q)
-    print "***********TOB****************"
-    for (p, q) in asks:
-        print "{0}x{1}".format(p, q)
+def start_1(configFile):
+    reactor.callWhenRunning(started)
+    reactor.run()
+
+######################################
+
+from pprint import pformat
+from twisted.web.client import Agent, readBody
+from twisted.web.http_headers import Headers
+from twisted.internet.protocol import Protocol
+
+def cbBody(body):
+    print 'Body: {0}'.format(body)
+
+def onBookL1Data(data):
+    print "onBookL1Data"
+    print data
+    print 'Response version: {0}'.format(data.version)
+    print 'Response code:    {0}'.format(data.code)
+    print 'Response phrase:  {0}'.format(data.phrase)
+    print 'Response headers: {0}'.format(pformat(list(data.headers.getAllRawHeaders())))
+    body = readBody(data)
+    body.addCallback(cbBody)
+    return body
+
+def shutdown(x):
+    print "shutdown"
+    reactor.stop()
+
+class CoinbaseRestClient:
+    def __init__(self, appName, apiUri):
+        self.appName = appName
+        self.apiUri  = apiUri
+
+    def _headers(self):
+        return Headers({
+            'Content-Type': ['application/json'],
+            'Accept'      : ['application/json'],
+            'User-Agent'  : [self.appName]
+        })
+
+    def _responseHandler(self, response, cb, errorCb):
+        if 200 != response.code:
+            errorCb(response)
+        else:
+            body = readBody(response)
+            body.addCallback(cb)
+            return body
+    
+    def _getData(self, req, cb, errorCb):
+        agent = Agent(reactor)
+        d = agent.request('GET', '{0}/{1}'.format(self.apiUri, req), self._headers(), None)
+        d.addCallback(lambda response: self._responseHandler(response, cb, errorCb))
+        return d
+
+    def getTime(self, cb, errorCb):
+        return self._getData('time', cb, errorCb)
+
+    def getCurrencies(self, cb, errorCb):
+        return self._getData('currencies', cb, errorCb)
+
+    def getProducts(self, cb, errorCb):
+        return self._getData('products', cb, errorCb)
+
+    def getTicker(self, product, cb, errorCb):
+        return self._getData('products/{0}/ticker'.format(product), cb, errorCb)
+
+    def getOrderBook(self, product, level, cb, errorCb):
+        return self._getData('products/{0}/book?level={1}'.format(product, level), cb, errorCb)
+        
+def printBody(body):
+    print 'Received body:'
+    print body
+
+def onError(x):
+    print "onError - aborting"
+    reactor.stop()
+
+def start_2(configFile):
+    crc = CoinbaseRestClient('coinbase-client', 'https://api.exchange.coinbase.com')
+    d   = crc.getTime(printBody, onError)
+    e   = crc.getCurrencies(printBody, onError)
+    f   = crc.getProducts(printBody, onError)
+    g   = crc.getTicker("BTC-USD", printBody, onError)
+    h   = crc.getOrderBook("BTC-USD", 2, printBody, onError)
+
+    reactor.run()
+
+    agent = Agent(reactor)
+    print vars(agent)
+    d = agent.request(
+        'GET', 
+        'https://api.exchange.coinbase.com/time',
+        Headers({'Content-Type': ['application/json'], 'Accept': ['application/json'], 'User-Agent': ['coinbase-client']}),
+        None
+    )
+    d.addCallback(onBookL1Data)
+    reactor.run()
 
 if __name__ == '__main__':
-    api_url = 'http://api.exchange.coinbase.com/'
-    product = "BTC-USD"
-    print "PRODUCTS"
-    print requests.get(api_url + 'products').json()
-    print ""
-    print "ORDER BOOK (LEVEL 1)"
-    print requests.get(api_url + 'products/' + product + '/book', params = {'level':1}).json()
-    print ""
-    print "ORDER BOOK (LEVEL 2)"
-    print requests.get(api_url + 'products/' + product + '/book', params = {'level':2}).json()
-    print ""
-    print "ORDER BOOK (LEVEL 3)"
-    bookL3 = requests.get(api_url + 'products/' + product + '/book', params = {'level':3}).json()
-    formatBookL3(bookL3)
-    print bookL3
-    print ""
-    print "PRODUCT TICKER"
-    print requests.get(api_url + 'products/' + product + '/ticker').json()
-    print ""
-    print "PRODUCT TRADES"
-    print requests.get(api_url + 'products/' + product + '/trades').json()
-    print ""
-    print "PRODUCT STATS"
-    print requests.get(api_url + 'products/' + product + '/stats').json()
-    print ""
-    print "CURRENCIES"
-    print requests.get(api_url + 'currencies').json()
-    print ""
-    print "TIME"
-    print requests.get(api_url + 'time').json()
-    print ""
+    if 2 != len(sys.argv):
+        print "Expected unique argument (configuration file)"
+    else:
+        configFile = sys.argv[1]
+        start_2(sys.argv[1])
 
